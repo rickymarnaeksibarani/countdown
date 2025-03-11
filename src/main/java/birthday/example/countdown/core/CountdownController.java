07890bdbac9ac9e40ffaa4d36d0742e39414dded
Ricky Marnaek Sibarani <ricky1931072@itpln.ac.id>
2025-03-11 08:26:46 +0700

    first commit

---
+++ src/main/java/birthday/example/countdown/core/CountdownController.java
@@ -0,0 +1,21 @@
+package birthday.example.countdown.core;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Controller
+public class CountdownController {
+    @GetMapping("/Praylin-Simarmata")
+//    http://localhost:8080/forPraylin/?eventDate=2025-03-10T14:48:00
+    public String countdownPage(@RequestParam(
+            name = "eventDate", required = false) String eventDate, Model model) {
+        LocalDateTime targetDate = eventDate != null ? LocalDateTime.parse(eventDate, DateTimeFormatter.ISO_LOCAL_DATE_TIME) : LocalDateTime.now().plusDays(7);
+        model.addAttribute("targetDate", targetDate);
+        return "countdown";
+    }
+}
